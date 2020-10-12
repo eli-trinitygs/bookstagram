de60d7734e65bbf31d748ec3f6c960fc8ac8acb9
Eli <elizabethtrinitygs@gmail.com>
2020-10-12 01:17:25 -0500

    creando vistas y jugando con patrones de urls , peticiones

--- bookstagram/urls.py
+++ bookstagram/urls.py
@@ -1,32 +1,31 @@
-"""bookstagram URL Configuration
+"""bookstagram URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/3.1/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 from django.urls import path
-from django.http import HttpResponse
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#]
-
-#---------------------------------------------
-
-#Todas las vistas reciben un request, y lo que regresa es un respuesta
-def hello_world(request):
-    return HttpResponse('Hello, world!')
+from bookstagram import views
 
 urlpatterns = [
-    path('hello-world/', hello_world )
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 ]
+
+"""urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+---------------------------------------------
+
+Todas las vistas reciben un request, y lo que regresa es un respuesta"""
 
---
+++ bookstagram/views.py
@@ -0,0 +1,16 @@
+""" Bookstagram views """
+#Django
+from django.http import HttpResponse
+#Utilities
+from datetime import datetime
+
+def hello_world(request):
+    """Return a greeting """
+    return HttpResponse('Oh, hi ! Current server time is {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %h:%M hrs')
+    ))
+
+def hi(request):
+    """Hi."""
+    numbers = request.GET['numbers']
+    return HttpResponse(str(numbers))
