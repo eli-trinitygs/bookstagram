3c093a5684b7802492014b21ceb0bc8ba6541f59
Eli <elizabethtrinitygs@gmail.com>
2020-10-27 22:21:28 -0600

    resolviendo ejercicio para ordenar numeros

--- bookstagram/views.py
+++ bookstagram/views.py
@@ -3,7 +3,7 @@
 from django.http import HttpResponse
 #Utilities
 from datetime import datetime
-
+import json
 #objeto request
 def hello_world(request):
     """Return a greeting """
@@ -15,7 +15,20 @@ def hello_world(request):
 #     import pdb; pdb.set_trace()
 #     return HttpResponse('Hola ke aze!')
 
-# def hi(request):
-#     """Hi."""
-#     numbers = request.GET['numbers']
-#     return HttpResponse(str(numbers))
+def hi(request):
+    """Hi."""
+    # numbers = request.GET['numbers']
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    #crar diccionario
+    data = {
+        'status': 'ok',
+        'numbers': 'sorte_ints',
+        'message': 'Integers sorted successfully!'
+    }
+    # import pdb; pdb.set_trace()
+    # return HttpResponse(str(numbers), content_type = 'application/json')
+    #implementando metodo json.dumps para traducir un diccionario a json
+    return HttpResponse(json.dumps(data,indent=4), 
+    content_type = 'application/json'
+    )
