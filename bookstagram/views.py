93e8c5b6c61f491ba364dec1c9d8b7a409906c22
Eli <elizabethtrinitygs@gmail.com>
2020-10-27 21:46:25 -0600

    creando un debugger -pdb- pone un debugger en la consola cada vez que se ejecute

--- bookstagram/views.py
+++ bookstagram/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 #Utilities
 from datetime import datetime
 
+#objeto request
 def hello_world(request):
     """Return a greeting """
     return HttpResponse('Oh, hi ! Current server time is {now}'.format(
@@ -11,6 +12,8 @@ def hello_world(request):
     ))
 
 def hi(request):
-    """Hi."""
-    numbers = request.GET['numbers']
-    return HttpResponse(str(numbers))
+    import pdb; pdb.set_trace()
+# def hi(request):
+#     """Hi."""
+#     numbers = request.GET['numbers']
+#     return HttpResponse(str(numbers))
